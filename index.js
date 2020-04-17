a1adc8f02866a5ec76247dd945e90a914b7306fd
halvardssm <halvard@simplyundoable.com>
2020-04-17 13:31:43 +0200

    added tsx

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const core = require('./presets/core')
-const jsx = require('./presets/jsx')
-const ts = require('./presets/ts')
+const core = require("./presets/core");
+const jsx = require("./presets/jsx");
+const ts = require("./presets/ts");
+const tsx = require("./presets/tsx");
 
 module.exports.configs = {
-	core: core,
-	jsx: jsx,
-	ts: ts
-}
+  core: core,
+  jsx: jsx,
+  ts: ts,
+  tsx: tsx,
+};
