1c81ed074c22b7c0de6047404d47a2f000e31c25
halvardssm <halvard@simplyundoable.com>
2020-04-17 16:22:39 +0200

    app folder updated

--- app/index.js
+++ app/index.js
@@ -1,19 +1,19 @@
-function fizzBuzzTs( limit ){
+function fizzBuzzJs( limit ){
  const result=[]
 	for( let i=1 ;i<=limit ;i++ ) {
 		if(i%3==0&&i%5==0) {
-				result.push ( 'FizzBuzz' )
+				result.push ( "FizzBuzz" )
 		}
 		else if(i%3== 0) {
-		result.push ( 'Fizz' )
+		result.push ( "Fizz" )
 		}
 		else if(i%5==0) {
-					result.push ( 'Buzz' )
+					result.push ( "Buzz" )
 		}
 		else {
 	result.push ( i.toString() )}  
 	}
-    console.log (`TS: [${ result.join (', ') }]` )
+    console.log (`TS: [${ result.join (", ") }]` )
 }
 
 
--- app/index.ts
+++ app/index.ts
@@ -1,20 +1,17 @@
 const fizzBuzzTs = (limit: number): void => {
 	const result: string[] = []
-	for (let i = 1; i <= limit; i++) {
-		if ( i % 3 == 0 && i % 5 == 0 ) {
-			result.push('FizzBuzz')
+	for (let i = 1;i <= limit;i++) {
+		if (i % 3 == 0 && i % 5 == 0) {
+			result.push("FizzBuzz")
 		}
-		else if ( i % 3 == 0 ) {
-			result.push('Fizz')
-		}
-		else if ( i % 5 == 0 ) {
-			result.push('Buzz')
-		}
-		else {
-			result.push(i.toString())
+		else if (i % 3 == 0) {
+			result.push("Fizz")}else if (i % 5 == 0) {
+			result.push("Buzz")
+		}else {
+			result.push( i.toString() )
 		}
 	}
-	console.log(`TS: [${result.join(', ')}]`)
+	console.log(`TS: [${ result.join (", ") }]`)
 }
 
-fizzBuzzTs(10)
+fizzBuzzTs( 10 )
