39b700e48e28d6f1a787a167f62ffd61ccab6570
halvardssm <halvard@simplyundoable.com>
2020-04-17 16:32:53 +0200

    updating app

---
+++ app/index.jsx
@@ -0,0 +1,20 @@
+function fizzBuzzJs( limit ){
+ const result=[]
+	for( let i=1 ;i<=limit ;i++ ) {
+		if(i%3==0&&i%5==0) {
+				result.push ( "FizzBuzz" )
+		}
+		else if(i%3== 0) {
+		result.push ( "Fizz" )
+		}
+		else if(i%5==0) {
+					result.push ( "Buzz" )
+		}
+		else {
+	result.push ( i.toString() )}  
+	}
+    console.log (`TS: [${ result.join (", ") }]` )
+}
+
+
+fizzBuzzTs(10)
